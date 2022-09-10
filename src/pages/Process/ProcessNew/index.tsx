a1436fa4b261e95c7e31fdb58b33d3886d60b091
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2022-09-10 16:53:35 -0300

    feat(Page: ProcessNew): :sparkles: Cria a página

---
+++ src/pages/Process/ProcessNew/index.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@chakra-ui/react";
+import { ProcessForm } from "../../../components/ProcessForm";
+
+export function ProcessNew() {
+	return (
+		<Box w="full">
+			<ProcessForm />
+		</Box>
+	);
+}
