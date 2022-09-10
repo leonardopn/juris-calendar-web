6b80a7799eb893ff30f4bad602af56aded6c82e9
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2022-09-09 21:03:59 -0300

    feat(Component: NavBar): :sparkles: Cria o componentes inicialmente

---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,66 @@
+import {
+	Link as ChakraLink,
+	Accordion,
+	AccordionButton,
+	AccordionIcon,
+	AccordionItem,
+	AccordionPanel,
+	Box,
+	VStack,
+} from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
+export function NavBar() {
+	return (
+		<VStack p={3} w="80" bg="purple.300" h="100vh" color="white">
+			<Accordion allowToggle w="full">
+				<AccordionItem flex="1">
+					<AccordionButton fontWeight="bold">
+						<ChakraLink as={Link} to="/">
+							Início
+						</ChakraLink>
+					</AccordionButton>
+				</AccordionItem>
+				<AccordionItem flex="1">
+					<AccordionButton>
+						<Box flex="1" textAlign="left" fontWeight="bold">
+							Processos
+						</Box>
+						<AccordionIcon />
+					</AccordionButton>
+					<AccordionPanel>
+						<ChakraLink as={Link} to="process/new">
+							Criar
+						</ChakraLink>
+					</AccordionPanel>
+				</AccordionItem>
+				<AccordionItem flex="1">
+					<AccordionButton>
+						<Box flex="1" textAlign="left" fontWeight="bold">
+							Tarefas
+						</Box>
+						<AccordionIcon />
+					</AccordionButton>
+					<AccordionPanel>
+						<ChakraLink as={Link} to="task/new">
+							Criar
+						</ChakraLink>
+					</AccordionPanel>
+				</AccordionItem>
+				<AccordionItem flex="1">
+					<AccordionButton>
+						<Box flex="1" textAlign="left" fontWeight="bold">
+							Memorandos
+						</Box>
+						<AccordionIcon />
+					</AccordionButton>
+					<AccordionPanel>
+						<ChakraLink as={Link} to="memorandum/new">
+							Criar
+						</ChakraLink>
+					</AccordionPanel>
+				</AccordionItem>
+			</Accordion>
+		</VStack>
+	);
+}
