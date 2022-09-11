db99393c019cff0dcb8b412fbafe4c549bac5700
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2022-09-11 17:13:16 -0300

    feat(Component: InputForm): :sparkles: Implementa mascara no componente

--- src/components/form/InputForm/index.tsx
+++ src/components/form/InputForm/index.tsx
@@ -1,20 +1,24 @@
 import { FormControl, FormErrorMessage, FormLabel, Input, InputProps } from "@chakra-ui/react";
 import { Control, FieldValues, Path, useController } from "react-hook-form";
+import { PatternFormat, PatternFormatProps } from "react-number-format";
+import { NumberFormatValues } from "react-number-format/types/types";
 
 interface InputFormProps<T extends FieldValues> extends InputProps {
 	label?: string;
 	name: Path<T>;
 	control: Control<T>;
+	mask?: Omit<PatternFormatProps, keyof InputProps>;
 }
 
 export function InputForm<T extends FieldValues>({
 	label,
 	control,
 	name,
+	mask,
 	...rest
 }: InputFormProps<T>) {
 	const {
-		field,
+		field: { ref, onChange, ...fieldRest },
 		fieldState: { error },
 	} = useController({ control, name });
 
@@ -27,7 +31,18 @@ export function InputForm<T extends FieldValues>({
 					{label}
 				</FormLabel>
 			)}
-			<Input {...field} {...rest} />
+			{mask ? (
+				<Input
+					as={PatternFormat}
+					{...mask}
+					{...rest}
+					{...fieldRest}
+					getInputRef={ref}
+					onValueChange={(e: NumberFormatValues) => onChange(e.formattedValue)}
+				/>
+			) : (
+				<Input {...fieldRest} {...fieldRest} ref={ref} onChange={onChange} />
+			)}
 			{!!error && <FormErrorMessage fontWeight={"bold"}>{error.message}</FormErrorMessage>}
 		</FormControl>
 	);
