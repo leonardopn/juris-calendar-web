9748ab7d0cb86bd87dd1103250b50f75abb3613f
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2022-09-11 21:48:51 -0300

    feat(Component: ProcessForm): :sparkles: Cria toda a lógica de formulário

--- src/components/ProcessForm/index.tsx
+++ src/components/ProcessForm/index.tsx
@@ -5,14 +5,80 @@ import {
 	Container,
 	Flex,
 	IconButton,
-	Input,
 	Stack,
+	Tooltip,
 	VStack,
 } from "@chakra-ui/react";
+import { DevTool } from "@hookform/devtools";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { Process } from "../../@types/Process";
+import { useAppDispatch } from "../../hooks/useAppDispatch";
+import { useAppSelector } from "../../hooks/useAppSelector";
+import { addProcess } from "../../store/slices/Process.slice";
+import { PROCESS_ID_MASK } from "../../utils/mask";
+import { PROCESS_VALIDATION } from "../../utils/yup";
 import { Card } from "../Card";
+import { InputForm } from "../form/InputForm";
 import { Header } from "../Header";
 
+interface AddProcessFormProps {
+	id: string;
+	part: string;
+	subject: string;
+}
+
 export function ProcessForm() {
+	const {
+		control,
+		handleSubmit,
+		reset,
+		formState: { isDirty, isSubmitting },
+	} = useForm<AddProcessFormProps>({
+		defaultValues: { id: "", part: "", subject: "" },
+		resolver: yupResolver(PROCESS_VALIDATION),
+	});
+	const dispatch = useAppDispatch();
+	const { process: processArray } = useAppSelector(state => state.process);
+	const navigate = useNavigate();
+
+	async function handleSubmitWithOutNavigate() {
+		await handleSubmit(onSubmit)();
+		navigate("/process/new");
+	}
+
+	const onSubmit: SubmitHandler<AddProcessFormProps> = async data => {
+		async function createProcess() {
+			const process: Process = {
+				...data,
+				createdAt: new Date(),
+				updatedAt: new Date(),
+			};
+
+			const findProcess = processArray.find(p => p.id === process.id);
+
+			if (!findProcess) {
+				dispatch(addProcess(process));
+				reset();
+				navigate("/");
+			} else {
+				throw new Error("Processo já cadastrado");
+			}
+		}
+
+		await toast.promise(createProcess, {
+			pending: "Criando processo",
+			success: "Processo criado com sucesso",
+			error: {
+				render({ data }) {
+					return data.message;
+				},
+			},
+		});
+	};
+
 	return (
 		<Stack flex="1" padding={10}>
 			<Container maxW={"4xl"}>
@@ -21,23 +87,52 @@ export function ProcessForm() {
 					title="Processos"
 					subTitle="Cadastre um processo"
 				/>
-				<Card mt="5!important">
+				<Card mt="5!important" as="form" onSubmit={handleSubmit(onSubmit)}>
 					<VStack spacing={3}>
-						<Input placeholder="Número" />
-						<Input placeholder="Parte" />
-						<Input placeholder="Assunto" />
+						<InputForm
+							label="Número do processo"
+							control={control}
+							name="id"
+							placeholder={PROCESS_ID_MASK.placeholder}
+							mask={PROCESS_ID_MASK}
+						/>
+						<InputForm
+							label="Parte do processo"
+							control={control}
+							name="part"
+							placeholder="Nome da parte"
+						/>
+						<InputForm
+							label="Assunto do processo"
+							control={control}
+							name="subject"
+							placeholder="Descontos indevidos"
+						/>
 						<Flex justify="space-between" w="full" mt="10!important">
-							<Button colorScheme={"red"}>Reset</Button>
+							<Button
+								colorScheme={"red"}
+								type="reset"
+								onClick={() => reset()}
+								disabled={!isDirty}>
+								Resetar
+							</Button>
 							<ButtonGroup isAttached alignSelf="end" colorScheme="main">
-								<Button>Criar</Button>
-								<IconButton
-									aria-label="adiciona mais um processo"
-									icon={<AddIcon />}
-								/>
+								<Button type="submit" isLoading={isSubmitting}>
+									Criar
+								</Button>
+								<Tooltip label="Cria mais um processo">
+									<IconButton
+										onClick={handleSubmitWithOutNavigate}
+										isLoading={isSubmitting}
+										aria-label="adiciona mais um processo"
+										icon={<AddIcon />}
+									/>
+								</Tooltip>
 							</ButtonGroup>
 						</Flex>
 					</VStack>
 				</Card>
+				<DevTool control={control} />
 			</Container>
 		</Stack>
 	);
