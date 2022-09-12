73472eee472d9d54aea55aaac05ed370257ff28d
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2022-09-11 21:19:47 -0300

    feat: :sparkles: Implementa o toastify e css para o mesmo

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider as ReduxProvider } from "react-redux";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { AppRoutes } from "./routes";
 import { store } from "./store/store";
 import { theme } from "./theme/theme";
@@ -11,6 +13,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 		<ReduxProvider store={store}>
 			<ChakraProvider theme={theme}>
 				<AppRoutes />
+				<ToastContainer />
 			</ChakraProvider>
 		</ReduxProvider>
 	</React.StrictMode>
