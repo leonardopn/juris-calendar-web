56af82adeb52b85d3a3f1c3243bd1fdc3bd6353b
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2022-09-13 20:12:57 -0300

    feat(Page: TaskNew): :sparkles: Cria a página

---
+++ src/pages/Task/TaskNew/index.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@chakra-ui/react";
+import { ProcessForm } from "../../../components/ProcessForm";
+
+export function TaskNew() {
+	return (
+		<Box w="full">
+			<ProcessForm />
+		</Box>
+	);
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "../pages/App";
 import { MemorandumNew } from "../pages/Memorandum/MemorandumNew";
 import { ProcessNew } from "../pages/Process/ProcessNew";
+import { TaskNew } from "../pages/Task/TaskNew";
 // import your route components too
 
 export function AppRoutes() {
@@ -11,6 +12,7 @@ export function AppRoutes() {
 				<Route path="/" element={<App />}>
 					<Route path="process/new" element={<ProcessNew />} />
 					<Route path="memorandum/new" element={<MemorandumNew />} />
+					<Route path="task/new" element={<TaskNew />} />
 				</Route>
 			</Routes>
 		</BrowserRouter>
