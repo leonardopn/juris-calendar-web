40d909c04d75b5df28ed70bcf849e70244feff08
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2022-09-13 19:48:22 -0300

    feat(Page: MemorandumNew): :sparkles: Cria a página

--- src/pages/Memorandum/MemorandumNew/index.tsx
+++ src/pages/Memorandum/MemorandumNew/index.tsx
@@ -1,5 +1,10 @@
 import { Box } from "@chakra-ui/react";
+import { MemorandumForm } from "../../../components/MemorandumForm";
 
 export function MemorandumNew() {
-	return <Box w="full" />;
+	return (
+		<Box w="full">
+			<MemorandumForm />
+		</Box>
+	);
 }
