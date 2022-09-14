37babaeafadab142d76210a7b21b94c231976f72
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2022-09-14 18:04:20 -0300

    feat(Type: Process): :label: Remove memorandums do tipo

--- src/@types/Process.ts
+++ src/@types/Process.ts
@@ -1,10 +1,7 @@
-import { Memorandum } from "./Memorandum";
-
 export interface Process {
 	id: string;
 	part?: string;
 	subject?: string;
-	memorandums: Memorandum[];
 	createdAt: Date;
 	updatedAt: Date;
 }
