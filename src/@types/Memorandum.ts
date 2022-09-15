762f9a924ce91989fff0221350d99c8c916b5c2d
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2022-09-14 22:51:30 -0300

    feat(Type: Memorandum): :label: Modifica a tipagem Memorandum e cria uma variante MemorandumToCreate

--- src/@types/Memorandum.ts
+++ src/@types/Memorandum.ts
@@ -1,9 +1,15 @@
 export interface Memorandum {
 	id: string;
+	numero:string;
 	destiny: string;
-	returnDate: string;//NOTE: Em formato ISO
+	taskId: string;
+	returnDate: string; //NOTE: Em formato ISO
 	isReturned: boolean;
 	returnId?: string;
 	createdAt: Date;
 	updatedAt: Date;
+}
+
+export interface MemorandumToCreate extends Omit<Memorandum, "taskId"> {
+	taskId?: string;
 }
