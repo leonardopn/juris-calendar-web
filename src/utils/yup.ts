1cbbc74f00c56a292d1a4d43e87adf872acea72a
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2022-09-19 17:41:59 -0300

    feat(Utils: Yup): :sparkles: Cria a TASK_VALIDATION

--- src/utils/yup.ts
+++ src/utils/yup.ts
@@ -7,7 +7,7 @@ interface MemorandumValidationProps {
 
 export const PROCESS_VALIDATION = yup
 	.object({
-		id: yup
+		number: yup
 			.string()
 			.required("O número é obrigatório")
 			.test("is-id", "O número deve conter 20 dígitos", value => {
@@ -30,9 +30,36 @@ export const MEMORANDUM_VALIDATION = (options?: MemorandumValidationProps) =>
 			returnDate: yup
 				.string()
 				.required("A data de retorno é obrigatória")
-				.test("isDate", "A data deve ser válida e posterior à hoje", value => {
+				.test("isDate", "A data deve ser válida", value => {
 					const dateToTest = dayjs(value);
 					return dateToTest.isValid();
 				}),
 		})
 		.required();
+
+export const TASK_VALIDATION = yup
+	.object({
+		initialDate: yup
+			.string()
+			.required("A data inicial é obrigatória")
+			.test("isDate", "A data deve ser válida", value => {
+				const dateToTest = dayjs(value);
+				return dateToTest.isValid();
+			}),
+		finalDate: yup
+			.string()
+			.required("A data final é obrigatória")
+			.test(
+				"isDate",
+				"A data deve ser válida e posterior a data inicial",
+				(value, context) => {
+					const dateToTest = dayjs(value);
+					const initialDate = dayjs(context.parent.initialDate);
+					return dateToTest.isValid() && dateToTest.isAfter(initialDate);
+				}
+			),
+		process: yup.object().nullable().required("O processo é obrigatório"),
+		category: yup.object().nullable().required("A categoria é obrigatória"),
+		status: yup.object().nullable().required("O status é obrigatório"),
+	})
+	.required();
