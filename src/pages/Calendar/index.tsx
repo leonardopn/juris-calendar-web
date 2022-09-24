81ab59d9cbd1393c96612230ba9a9caab2e65191
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2022-09-23 21:34:56 -0300

    feat(Page: Calendar): :sparkles: Cria rota e página

---
+++ src/pages/Calendar/index.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@chakra-ui/react";
+import { TaskCalendar } from "../../components/TaskCalendar";
+
+export function Calendar() {
+	return (
+		<Box w="full">
+			<TaskCalendar />
+		</Box>
+	);
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "../pages/App";
+import { Calendar } from "../pages/Calendar";
 import { MemorandumNew } from "../pages/Memorandum/MemorandumNew";
 import { ProcessNew } from "../pages/Process/ProcessNew";
 import { TaskNew } from "../pages/Task/TaskNew";
@@ -10,6 +11,7 @@ export function AppRoutes() {
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<App />}>
+					<Route index element={<Calendar />} />
 					<Route path="process/new" element={<ProcessNew />} />
 					<Route path="memorandum/new" element={<MemorandumNew />} />
 					<Route path="task/new" element={<TaskNew />} />
